d89b5ba7ba3570522627ebeabecb8c16856c3646
Ouail Amarir <ouailamarir@gmail.com>
2025-01-18 00:58:34 +0100

    Ajout de la configuration de next-sitemap, création de fichiers sitemap et robots.txt, et suppression de l'ancien fichier next-sitemap.js

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+// next-sitemap.config.js
+module.exports = {
+    siteUrl: 'https://csseducteur.club', // Remplacez par votre propre domaine
+    generateRobotsTxt: true, // Génère un fichier robots.txt (optionnel)
+    changefreq: 'daily', // Fréquence des mises à jour (daily, weekly, monthly, etc.)
+    priority: 0.7, // Priorité des pages
+    sitemapSize: 5000, // Nombre maximum de liens par sitemap
+  }
+  
--- next-sitemap.js
+++
@@ -1,7 +0,0 @@
-// next-sitemap.js
-export const siteUrl = 'https://csseducteur.club';
-export const generateRobotsTxt = true;
-export const changefreq = 'daily';
-export const priority = 0.7;
-export const sitemapSize = 5000;
-  
