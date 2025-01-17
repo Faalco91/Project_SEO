8ab6c5e4f168126a5155c1836ad79435228d1cf1
bobiboriss <celineartemis2@gmail.com>
2025-01-17 20:41:04 +0100

    Revert "update**3"
    This reverts commit 5aefe2c65ef96c935323b6719f89c67d2f871c8e.

--- src/app/services/metadata.js
+++
@@ -1,5 +0,0 @@
-export const metadata = {
-  title: 'Services de CSSéducteur | Coaching en Séduction et Développement Personnel',
-  description: 'Explorez les services de CSSéducteur : coaching individuel, coaching en groupe, ateliers pratiques, et conférences. Transformez votre vie grâce à nos techniques de séduction et de développement personnel.',
-  keywords: 'CSSéducteur, coaching séduction, coaching individuel, coaching groupe, ateliers pratiques, conférences, développement personnel',
-}; 
